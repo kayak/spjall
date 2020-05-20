fd816b8d657589d56a4f9991d0665cd435679c1b
Paul Thordarson <thor@kayak.com>
2020-05-20 15:01:04 -0400

    Completed doc comments for WebApi.

--- src/main/scala/com/r9/spjall/web/WebApi.scala
+++ src/main/scala/com/r9/spjall/web/WebApi.scala
@@ -15,6 +15,16 @@ import scala.concurrent.{ ExecutionContext, Future }
  * @author Paul Thordarson - thor@kayak.com
  */
 trait WebApi {
+  /**
+   * Performs a singe Web API Request against a web API
+   *
+   * @param r            An `HttpRequest` instance used for accessing the API.
+   * @param contentField This is the name of the field in the API response that is expected to
+   *                     to have the actually list of returned content. Used in parsing.
+   * @param as           ActorSystem used to execute this function call.
+   * @tparam A           The type of the element expected to be returned by the API call.
+   * @return             An `ApiResponse` item of A.
+   */
   def performApiRequest[A: Decoder](r: HttpRequest, contentField: Option[String] = None)(implicit as: ActorSystem): Future[ApiResponse[A]] = {
     implicit val materializer: ActorMaterializer = ActorMaterializer()
     implicit val executionContext: ExecutionContext = as.dispatcher
@@ -31,15 +41,25 @@ trait WebApi {
   }
 
   /**
-   * Performs paged Web API Request. Slack has support
-   * @param rg
-   * @param contentField
-   * @param carry
-   * @param w
-   * @param m
-   * @param as
-   * @tparam A
-   * @return
+   * Performs a paged web API Request. Many Slack API calls have page limits and support paging
+   * through the results. This function performs a series of API calls that pages through a
+   * given endpoint and returns the complete set of items from all of the requests. This function
+   * operates recursively by passing the meta from a returned API call (which contains the next
+   * page index) on until there is no next page.
+   *
+   * @param rg           A function that takes an optional meta field from a previous function
+   *                     call and returns an HttpRequest.
+   * @param contentField This is the name of the field in the API response that is expected to
+   *                     to have the actually list of returned content. Used in parsing.
+   * @param carry        This is a Seq containing an aggregation of previously returned elements.
+   *                     Used in performing a tail recursive call to pass the "results so far"
+   *                     on to the call.
+   * @param w            An aggregation of warnings returned by the API so far.
+   * @param m            An optional meta returned by the previous call. Passed to `rg` to generate
+   *                     the next request if needed.
+   * @param as           ActorSystem used to execute this function call.
+   * @tparam A           The type of the elements expected to be returned by the API call.
+   * @return             An `ApiResponse` item of Seq[A].
    */
   def performPagedApiRequest[A: Decoder](
     rg: Option[Meta] => HttpRequest,
