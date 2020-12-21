fb69627d3e801a2e70ba5935f48a90cfa7e9f704
Paul Thordarson <thor@kayak.com>
2020-12-21 11:49:02 -0500

    Link formatting tweaks.

--- src/main/scala/com/r9/spjall/format/package.scala
+++ src/main/scala/com/r9/spjall/format/package.scala
@@ -1,8 +1,9 @@
 package com.r9.spjall
 
 package object format {
-  def formatCall(num: String): String = formatLink(num.replaceAll("[ \\+\\-]", ""), num, "tel:")
-  def formatSms(num: String): String = formatLink(num.replaceAll("[ \\+\\-]", ""), num, "sms://")
+  def formatCall(num: String): String = formatLink(num.replaceAll("[ \\+\\-()]", ""), num, "tel:")
+  def formatSms(num: String): String = formatLink(num.replaceAll("[ \\+\\-()]", ""), num, "sms://")
   def formatEmail(email: String): String = formatLink(email, email, "mailto:")
   def formatLink(link: String, text: String, protocol: String): String = s"<$protocol$link|$text>"
+  def formatRaw(link: String): String = s"<$link>"
 }
